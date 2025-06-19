d90c2e4438b19a580b196eff6640b962be061bce
Doritos-wappi5051 <23fi065@ms.dendai.ac.jp>
2025-06-19 13:13:29 +0900

    main.py

--- main.py
+++ main.py
@@ -27,8 +27,12 @@ def index():
         </head>
         <body>
             <h1>Look ma! HTML!</h1>
-            <h4>
+            <h4>単位ください</h4>
         </body>
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+    @app.post("/present")
+async def give_present(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
